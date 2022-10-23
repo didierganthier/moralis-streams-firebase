54672eec7ff9fe4362984f90dffc31c6b03b7bc6
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-22 23:44:20 -0400

    Lisstening to Blockchain Events using Firestore

---
+++ public/env.js
@@ -0,0 +1,9 @@
+window.env = {
+  "API_KEY": "AIzaSyC4uYJCGZ_TN4bmynhjZ0r0i1mXDjlvTTs",
+  "PROJECT_ID": "moralis-stream",
+  "BUCKET_NAME": "moralis-stream.appspot.com",
+  "AUTH_DOMAIN": "moralis-stream.firebaseapp.com",
+  "MESSAGING_SENDER_ID": "1015433097551",
+  "APP_ID": "1:1015433097551:web:7aab004a4e2b8314bad53a",
+  "MEASUREMENT_ID": "G-G58X5TGF0D"
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  query,
+  collection,
+  onSnapshot,
+  getFirestore,
+} from "firebase/firestore";
+import env from "react-dotenv";
+
+const { API_KEY, PROJECT_ID, BUCKET_NAME, AUTH_DOMAIN, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } = env;
+
+console.log(API_KEY);
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: BUCKET_NAME,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function App() {
+  const [txs, setTxs] = useState(null);
+
+  useEffect(() => {
+    console.log("start");
+    const q = query(collection(db, "moralis/txs/Polygontestnet"));
+    console.log("query has been setup");
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const tempTxs = [];
+      querySnapshot.forEach((doc) => {
+        tempTxs.push(doc.data());
+      });
+      setTxs(tempTxs);
+    });
+
+
+    //Stop realtime updates:
+    //unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>🔥 Firebase Moralis Streams Extension 🔥</p>
+            <table className="table">
+              <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Amount</th>
+                <th>Block</th>
+                <th>Hash</th>
+                <th>Time</th>
+              </tr>
+              {txs?.map((e,i)=>{
+                return(
+                  <tr key={i}>
+                    <td>{e.fromAddress}</td>
+                    <td>{e.toAddress}</td>
+                    <td>{e.value / 1E18} Matic</td>
+                    <td>{e.blockNumber}</td>
+                    <td>{e.hash}</td>
+                    <td>{e.timestamp}</td>
+                  </tr>
+                )
+              })}
+            </table>
       </header>
     </div>
   );
 }
-
 export default App;
